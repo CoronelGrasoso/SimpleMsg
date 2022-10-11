f28e30974f80ed50bdb21925373f8d850fcdd797
CoronelGrasoso <109289702+CoronelGrasoso@users.noreply.github.com>
2022-10-11 21:22:45 +0200

    Create Utils.java

---
+++ me/shrek820/simplemsg/Utils.java
@@ -0,0 +1,13 @@
+package me.shrek820.simplemsg;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+	
+	public String color(String s) {
+		
+		return ChatColor.translateAlternateColorCodes('&', s);
+		
+	}
+	
+}
